d52190b82cf2b82f502d0e65cdc19277abcb6d17
locluklak <dtanloc.j@gmail.com>
2026-04-27 17:21:08 +0700

    Implement dynamic attribute access in LoRASWAG: add __getattr__ method to delegate attribute retrieval to the wrapped model

--- src/swag.py
+++ src/swag.py
@@ -134,3 +134,9 @@ class LoRASWAG(nn.Module):
 
     def forward(self, *args, **kwargs):
         return self.model(*args, **kwargs)
+
+    def __getattr__(self, name):
+        try:
+            return super().__getattr__(name)
+        except AttributeError:
+            return getattr(self.model, name)
